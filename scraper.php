4cc69e853b78e9d53cf1d8182a3ee7a1042d3c08
Bradbury83 <emobi.adw@gmail.com>
2016-01-22 18:34:06 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-$html = scraperWiki::scrape("http://www.gsmarena.com/samsung_i9300_galaxy_s_iii-4238.php");
+$html = scraperWiki::scrape("http://www.gsmarena.com/samsung_galaxy_a7_%282016%29-7759.php");
 //print $html . "\n";
 
 $dom = new simple_html_dom();
